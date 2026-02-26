4fdedd418213c803c1eb2b8c154556543b7add8c
Christopher <carpiocris94@gmail.com>
2026-02-26 14:30:20 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaRepositorioVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este cambio va desde Visual Studio en C#");
+        }
+    }
+}
