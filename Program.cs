d80691ab2d6b97d20db9e8f30cbab80492e94a85
Christopher <carpiocris94@gmail.com>
2026-02-26 21:08:01 -0500

    eliminado de linea 3

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace PruebaRepositorioVisualStudio
             Console.WriteLine("Este cambio va desde Visual Studio en C#");
             Console.WriteLine("Un nuevo print para la prueba desde visual studio");
 
-            Console.WriteLine("Otro print adicional a la prueba con C#");
 
 
         }
